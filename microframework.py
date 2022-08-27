b9a1ffd442f0149fa4da9836e229b1c3628db4b3
karthikeyan_01 <karthikerathore@gmail.com>
2022-08-27 16:10:24 +0530

    server is running

--- microframework.py
+++ microframework.py
@@ -13,10 +13,20 @@ class Microframework(object):
   session_cookie_name = 'session'
   
   def __init__(self):
+    # debug option
+    self.debug = None
+    # route these functions
     self.view_functions = {}
   
-  def run(self):
-    print('running microframework application here')
+  def run(self, host='localhost', port=5000, **options):
+    # https://werkzeug.palletsprojects.com/en/2.2.x/tutorial/#step-2-the-base-structure
+    # run application on local developement server 
+    from werkzeug.serving import run_simple
+    if 'debug' in options:
+      self.debug = options.pop('debug')
+    options.setdefault('use_debugger', self.debug)
+    options.setdefault('use_reloader', self.debug)
+    return run_simple(host, port, self, **options)
   
   def __call__(self):
     print("call function starts here")
@@ -36,6 +46,9 @@ if __name__ == "__main__":
   # Microframework application
   elif os.getenv('MICROFRAMEWORK') is not None:
     app = Microframework()
+    if os.getenv('DEBUG') is not None:
+      app.run(debug=True)
+    # run without debug mode.
     app.run()
 
   # Missing  ENV var
